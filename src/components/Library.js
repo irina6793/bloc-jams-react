a78c774e5be88a2e7b788e45a90a600265e2e965
Irina Sverzhanovskaya <irina6793@yahoo.com>
2018-05-21 21:53:44 -0700

    addition

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
+import albumData from './../data/albums';
 
 class Library extends Component {
+  constructor(props) {
+     super(props);
+     this.state = { albums: albumData };
+   }
+
    render() {
     return (
       <section className='library'>
-        Library will go here
+        {
+         this.state.albums.map( (album, index) =>
+           <div key={index} >
+             <img src={album.albumCover} alt={album.title} />
+               <div>{album.title}</div>
+               <div>{album.artist}</div>
+               <div>{album.songs.length} songs</div>
+           </div>
+         )
+       }
       </section>
      );
    }
