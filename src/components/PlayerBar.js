83481ec4c75c43cde2cb7dd0c30d3601fd4cc402
Irina Sverzhanovskaya <irina6793@yahoo.com>
2018-05-27 20:47:47 -0700

    new addition

--- src/components/PlayerBar.js
+++ src/components/PlayerBar.js
@@ -6,14 +6,14 @@ import React, { Component } from 'react';
      return (
        <section className="player-bar">
          <section id="buttons">
-           <button id="previous">
-             <span className="ion-skip-backward"></span>
+           <button id="previous" onClick={this.props.handlePrevClick}>
+           <span className="ion-skip-backward"></span>
            </button>
            <button id="play-pause" onClick={this.props.handleSongClick}>
              <span className={this.props.isPlaying ? 'ion-pause' : 'ion-play'}></span>
            </button>
-           <button id="next">
-             <span className="ion-skip-forward"></span>
+           <button id="next" onClick={this.props.handleNextClick}>
+             <span className="ion-skip-forward"></span> 
            </button>
          </section>
          <section id="time-control">
